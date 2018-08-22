a4706b9741b376b18fecd3288b9fbd09e588ddd0
Arielle D <domingu4@gmail.com>
2018-08-22 15:35:36 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,3 +1,8 @@
 #write your code here
 def countdown(countdown_output)
+  while countdown_output > 0
+    puts "#{countdown_output} SECOND(S)!"
+    countdown_output -= 1
+  end
+  HAPPY NEW YEAR!
 end
